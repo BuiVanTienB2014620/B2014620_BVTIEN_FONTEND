2b3404eafdf47325350c0369d9c5f0e35a6f4691
B2014620 <tienb2014620@student.ctu.edu.vn>
2023-09-07 22:24:08 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import './assets/form.css';
+import './assets/main.css';
 
-createApp(App).mount('#app')
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createApp } from "vue";
+import App from './App.vue';
+import router from "./router";
+
+createApp(App).use(router).mount("#app");
+
+// createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url';
 
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
-import { Server } from 'node:http';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,5 +15,11 @@ export default defineConfig({
   },
   server: {
       port: 3001,
+      proxy: {
+        "/api": {
+          target: "http://localhost:3000/",
+          changeOrigin: true,
+        },
+      }
   },
 });
