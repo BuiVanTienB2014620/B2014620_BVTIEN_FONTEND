bafadeec203c4ff9a2ff4c636612e6adba978631
B2014633 <vinhb2014633@student.ctu.edu.vn>
2023-10-12 21:08:19 +0700

    Tao trang cap nhat lien he

---
+++ src/views/ContactNew.vue
@@ -0,0 +1,45 @@
+
+<template>
+<div class="page">
+  <h4>Thêm mới liên hệ</h4>
+  <ContactFormNew @submit:contact="createContact" />
+</div>
+<p>{{ message }}</p>
+</template>
+
+<script>
+import ContactFormNew from "@/components/ContactFormNew.vue";
+import ContactService from "@/services/contact.service.js";
+
+export default {
+  components: {
+    ContactFormNew,
+  },
+  props: {
+    contact: { type: Object, require: true },
+  },
+  
+  data() {
+    return {
+     
+      message: "",
+    };
+  },
+  methods: {
+    
+    async createContact(data) {
+      // Hiển thị cửa sổ xác nhận
+      const confirmed = window.confirm("Bạn có thêm liên hệ mới?");
+
+      if (confirmed) {
+        try {
+          await ContactService.create(data);
+          this.message = "Thêm liên hệ thành công";
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    },
+  },
+};
+</script>
